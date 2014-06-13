7a78d08db4795312f13633f6f60fe66df19d1b53
Emma Gardner <emmagardner@outlook.com>
2014-06-13 15:01:09 -0400

    add apts attribute
    * creates the instance var apts as an empty array to accept "many apts"
    
    * is NOT set to accessor so it can not be set via methods.

--- lib/building.rb
+++ lib/building.rb
@@ -1,16 +1,22 @@
 require 'pry'
 
-# has an address
-# has many apartments (array)
-# the list of apartments should not be modified directly (bonus: actually prevent it from being modified directly)
+
+
+
 # has a method to add an apt
 # has a method to remove a specific apartment by its number, which raises an error if the number is not found or the apartment currently has any tenants (bonus: allow overriding this constraint)
 # has a total square footage, calculated from all apartments
 # has a total monthly revenue, calculated from all apartment rents
 # has a list of tenants, pulled from the tenant lists of all apartments
 # has a method to retrieve all apartments grouped by credit rating (bonus: sort the groups by credit score)
+attr_accessor address:
 
-class Building
+class Building(address:)
+  # has an address
+  @address = address
+  # has many apartments (array)
+  # the list of apartments should not be modified directly (bonus: actually prevent it from being modified directly)
+  @apts = []
 
 end
 
