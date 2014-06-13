9bae3ed1664922f22c88d86fa84f8dee62b2182a
Jodi Detch <jodidetch@gmail.com>
2014-06-13 15:37:54 -0400

    Added method to add apts

--- bin/test_building.rb
+++ bin/test_building.rb
@@ -5,3 +5,5 @@ my_building = Building.new('123 Main Street')
 #msg = my_building.apartments ? "has" : "does not have"
 
 puts "My building is located at #{my_building.address} and has #{my_building.apartments} apartments."
+
+puts "I just built an addition and now have #{my_building.add_apts(3)} apartments."
