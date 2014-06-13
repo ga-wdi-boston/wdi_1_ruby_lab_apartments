52e8dca23c7030d3af05b5f261b33adf73fb1e03
Devin Dreszer <devindreszer@gmail.com>
2014-06-13 17:08:57 -0400

    Update building class
    * To include method to retrieve apartments by credit rating

--- lib/building.rb
+++ lib/building.rb
@@ -10,12 +10,14 @@ class Building
     apartments.push(apartment)
   end
 
-  def remove_apartment(number)
+  def remove_apartment(number, has_tenants: false)
     apartment = apartments.find { |apartment| apartment.number == number}
-    if !apartment.nil? && !apartment.tenants.any?
-      apartments.delete(apartment)
-    else
+    if apartment.nil?
       raise "That apartment does not exist in the building."
+    elsif apartment.tenants.any? != has_tenants
+      raise "That aparment has tenants. Please specify has_tenants true to delete anyways."
+    else
+      apartments.delete(apartment)
     end
   end
 
@@ -35,4 +37,13 @@ class Building
     single_list
   end
 
+  def apartments_by_credit_rating
+    apartments_by_credit = {}
+    apartments_by_credit["excellent"] = ((apartments.select { |apartment| apartment.credit_rating == "excellent"}).sort_by { |apartment| apartment.credit_score }).reverse
+    apartments_by_credit["great"] = ((apartments.select { |apartment| apartment.credit_rating == "great"}).sort_by { |apartment| apartment.credit_score }).reverse
+    apartments_by_credit["good"] = ((apartments.select { |apartment| apartment.credit_rating == "good"}).sort_by { |apartment| apartment.credit_score }).reverse
+    apartments_by_credit["mediocre"] = ((apartments.select { |apartment| apartment.credit_rating == "mediocre"}).sort_by { |apartment| apartment.credit_score }).reverse
+    apartments_by_credit["bad"] = ((apartments.select { |apartment| apartment.credit_rating == "bad"}).sort_by { |apartment| apartment.credit_score }).reverse
+    apartments_by_credit
+  end
 end
