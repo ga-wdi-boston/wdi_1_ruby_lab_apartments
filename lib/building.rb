1610e525ca724c88d650142d812dd1d24eaa6456
krk324 <krk32488@gmail.com>
2014-06-13 17:36:42 -0400

    Add add_apartment , delete_apartment methods

--- lib/building.rb
+++ lib/building.rb
@@ -1,15 +1,28 @@
 require_relative '../lib/apartment.rb'
 class Building
-  attr_accessor = :name
-  attr_reader = :apartments
+  attr_accessor :address
+  attr_reader :apartments
 
   def initialize(address)
     @address = address
-    @apartments = []
+    @apartments = Array.new(0)
   end
 
-  # def add()
-  #   @apartments <<
+  def add_apartment(number,rent,square_footage,bedroom,bathroom)
+    @apartments << Apartment.new(number,rent,square_footage,bedroom,bathroom)
+  end
+
+  def delete_apartment(number)
+    self.apartments.delete_if {|apartment| apartment.number == number}
+    #can't detelete by reference id
+  end
+
+  # def delete_all_tenant
+  #   self.tenants.clear
+  # end
+
+  # def credit_average
+  #   self.tenants.map{|score| score.credit_score}.reduce(0){|sum,x| sum + x } / self.tenants.length
   # end
 
 end
