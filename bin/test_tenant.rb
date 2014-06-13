2e8fa42ec183b03583d16bc1c6dc536d27a2575a
Jodi Detch <jodidetch@gmail.com>
2014-06-13 16:40:19 -0400

    Added Tenant class and successfully tested new instance

---
+++ bin/test_tenant.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/tenant.rb'
+
+tenant = Tenant.new('Jodi', 28, 720)
+
+
+puts "My tenant is #{tenant.name}, who is #{tenant.age} years old and has a credit score of #{tenant.credit_score}."
