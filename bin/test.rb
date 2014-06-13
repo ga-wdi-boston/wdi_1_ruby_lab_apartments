7d3023d0253a7fc6d774a85b791bd0f43b8e6d29
krk324 <krk32488@gmail.com>
2014-06-13 15:18:12 -0400

    Create test.rb and building.rb.   *initialize methods for building.rb

---
+++ bin/test.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/building.rb'
+
+puts Building.new('417 river grove')
+#raise "haha here's my error"
---
+++ lib/building.rb
@@ -0,0 +1,14 @@
+class Building
+  attr_accessor = :name
+  attr_reader = :apartments
+
+  def initialize(address)
+    @address = address
+    @apartments = []
+  end
+
+  # def add()
+  #   @apartments <<
+  # end
+
+end
