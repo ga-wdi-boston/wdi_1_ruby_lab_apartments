0ab4344159f4f958b04ced35d00ad96203d011ff
Emma Gardner <emmagardner@outlook.com>
2014-06-13 15:03:33 -0400

    oops- my class set up was all messed up- i made corrections to fix this

--- lib/building.rb
+++ lib/building.rb
@@ -9,15 +9,18 @@ require 'pry'
 # has a total monthly revenue, calculated from all apartment rents
 # has a list of tenants, pulled from the tenant lists of all apartments
 # has a method to retrieve all apartments grouped by credit rating (bonus: sort the groups by credit score)
-attr_accessor address:
 
-class Building(address:)
-  # has an address
-  @address = address
-  # has many apartments (array)
-  # the list of apartments should not be modified directly (bonus: actually prevent it from being modified directly)
-  @apts = []
 
+class Building
+  attr_accessor address:
+
+  def initialize(address:)
+    # has an address
+    @address = address
+    # has many apartments (array)
+    # the list of apartments should not be modified directly (bonus: actually prevent it from being modified directly)
+    @apts = []
+  end
 end
 
 binding.pry
