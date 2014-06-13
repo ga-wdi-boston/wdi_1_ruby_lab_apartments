a92e2d972c46420b132aac72727e67f976d1ca66
Jake Austin <jakeaust07@gmail.com>
2014-06-13 16:29:28 -0400

    define building class

---
+++ lib/building.rb
@@ -0,0 +1,33 @@
+# Jake Austin
+# WDI Boston June comprehensive assessment
+# Jun 13, 2014
+
+class Building
+
+  attr_reader :address, :apartments
+
+  def initialize(address:)
+    @Address = address
+    @apartments = []
+  end
+
+  def add_appt(appt)
+  end
+
+  def remove_appt(appt_num)
+  end
+
+  def sq_footage
+  end
+
+  def monthly_revenue
+  end
+
+  def tenants
+  end
+
+  def appts_by_cred
+  end
+
+
+end
