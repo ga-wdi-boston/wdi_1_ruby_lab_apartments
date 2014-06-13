be32671901269bae34999628e159823eec17437f
dmcgann1 <dmcgann1@gmail.com>
2014-06-13 15:41:08 -0400

    Method to add tenant working

--- bin/test.rb
+++ bin/test.rb
@@ -2,12 +2,17 @@ require_relative '../lib/apartment.rb'
 require_relative '../lib/building.rb'
 require_relative '../lib/tenant.rb'
 
+#instance of tenant method and methods to test
 mike = Tenant.new(name: 'mike', age: '24', credit_score: 710)
 
 puts mike.inspect
 puts mike.credit_rating
 
-donnybrook = Apartment.new(number: 5, rent: 1000, square_ft: 2000, beds: 1, baths: 1)
+#instance of apartment method and methods to test
+donnybrook = Apartment.new(number: 5, rent: 1000, square_ft: 2000, beds: 0, baths: 1)
 
 puts donnybrook.inspect
+
+donnybrook.add_tenant(mike)
+puts donnybrook.tenants
 
--- lib/apartment.rb
+++ lib/apartment.rb
@@ -6,4 +6,13 @@ class Apartment
     @number, @rent, @square_ft, @beds, @baths, @tenants = number, rent, square_ft, beds, baths, []
   end
 
+  def add_tenant(prospect)
+    #prospect.credit_rating != "bad" && beds - tenants.count != 0 ? tenants << prospect : raise "cannot add tenant"
+    if prospect.credit_rating != 'bad' && tenants.count < beds
+      tenants << prospect
+    else
+      raise "Cannot add tenant"
+    end
+  end
+
 end
