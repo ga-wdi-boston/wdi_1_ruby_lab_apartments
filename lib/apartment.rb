1e6f476a430bc3f85e2ee2f7e03d091e2830860f
Max Kohl <maxkohl810@gmail.com>
2014-06-13 17:56:25 -0400

    find both credit score and credit rating for an apartment

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,5 +1,6 @@
 class Apartment
-  attr_reader :tenants, :bedrooms
+  attr_reader :bedrooms
+  attr_accessor :tenants
 
   def initialize (number:, rent:, square_footage:, bedrooms: 1, bathrooms: 0)
     @number, @rent, @square_footage, @bedrooms, @bathrooms, @tenants = number, rent, square_footage, bedrooms, bathrooms, []
@@ -14,5 +15,34 @@ class Apartment
       @tenants.push(tenant)
     end
   end
+
+  def remove_all_tenants
+    tenants.clear
+  end
+
+  def credit_score
+     average_credit = tenants.map { |tenant| tenant.credit_score }.reduce(0, &:+) / tenants.count
+  end
+
+  def credit_rating
+    if credit_score >= 760
+      credit_rating = 'excellent'
+    elsif (725..759).member?(credit_score)
+      credit_rating = 'great'
+    elsif (660..724).member?(credit_score)
+      credit_rating = 'good'
+    elsif (560..659).member?(credit_score)
+      credit_rating = 'mediocre'
+    else
+      credit_rating = 'bad'
+    end
+    credit_rating
+  end
+
+  # def remove_a_tenant(tenant, name: '')
+  #   if tenants.include?(tenant)
+  #     tenants.delete(tenant)
+  #   elsif tenants
+  # end
 
 end
