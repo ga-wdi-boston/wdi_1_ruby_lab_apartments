a4d1f966d4f5cbc14a8c57de41af1b642aabad8e
sarahchristine913 <s.mele13@gmail.com>
2014-06-13 14:45:04 -0400

    Initial Commit
    *

---
+++ lib/tenant.rb
@@ -0,0 +1,8 @@
+# Tenant
+# * has a name, age, and credit score
+# * has a credit rating, calculated from the credit score as follows:
+#   * 760 or higher is "excellent"
+#   * 725 or higher is "great"
+#   * 660 or higher is "good"
+#   * 560 or higher is "mediocre"
+#   * anything lower is "bad"
