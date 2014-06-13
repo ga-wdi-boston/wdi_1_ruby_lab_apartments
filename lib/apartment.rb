f902414b2a5ad25d9c4fd7c66a1fc0bbf2937dc9
Julie <juliesof@gmail.com>
2014-06-13 15:28:37 -0400

    Created new docs for each class and test case
    *wrote the initialize var for apartment

---
+++ lib/apartment.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Apartment
+
+  attr_accessor :rent, :num_tenants #not sure if tenants should go here
+  attr_reader :apt_num, :sq_footage, :num_bedrooms, :num_bathrooms
+
+  def initialize
+
+  end
+
+
+
+
+
+
+
+
+
+end
