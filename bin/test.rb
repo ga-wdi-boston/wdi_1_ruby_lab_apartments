f5dfe690ef31d0bad64a24b21b954e9edc1fa417
krk324 <krk32488@gmail.com>
2014-06-13 16:28:25 -0400

    Add add_tenant method for Apartment class.

--- bin/test.rb
+++ bin/test.rb
@@ -1,9 +1,24 @@
 require_relative '../lib/building.rb'
-require_relative '../lib/Apartment.rb'
+require_relative '../lib/apartment.rb'
+require 'pry'
+
+#Tenant
+tenant1 = Tenant.new('Tom',24,720)
+puts tenant1.credit_check
+
+#test apartment
+Apartment1 = Apartment.new(417,900,30,3,1)
+
+#Apartment's test add tenant
+Apartment1.add_tenant('Tom',25,900)
+Apartment1.add_tenant('Tom',25,900)
+Apartment1.add_tenant('Tom',25,900)
+Apartment1.add_tenant('Tom',25,900)
+
+puts Apartment1.inspect
+
+pry.binding
+
 
 # test building
 puts Building.new('417 river grove')
-
-#test apartment
-puts Apartment.new(417,900,30,3,1)
-#raise "haha here's my error"
--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,14 +1,26 @@
+require_relative '../lib/tenant.rb'
+
 class Apartment
 
   attr_accessor :number, :rent, :square_footage, :bedroom, :bathroom
+  attr_reader :tenants
 
   def initialize(number,rent,square_footage,bedroom,bathroom)
     @number,@rent,@square_footage = number,rent,square_footage
     @bedroom,@bathroom = bedroom,bathroom
-    @tenants = []
+    @tenants = Array.new(0)
   end
 
+  def add_tenant(name,age,credit_score)
+    tenant = Tenant.new(name,age,credit_score)
+    if tenant.credit_check == 'bad'
+      raise "bad credit rating"
+    elsif @tenants.length + 1 > bedroom
+      raise "Tenants exceed bedroom number"
+    end
+    @tenants << tenant
 
+  end
 
 end
 
