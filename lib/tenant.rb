44d0adae1f7a4cca44df5483ebaec2994f00050d
krk324 <krk32488@gmail.com>
2014-06-13 15:53:13 -0400

    Tenant class - initialized and add credit_check method.

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,3 +1,23 @@
 class Tenant
 
+  attr_accessor :name,:age,:credit_score
+
+  def initialize (name,age,credit_score)
+    @name,@age,@credit_score = name,age,credit_score
+  end
+
+  def credit_check
+    if credit_score >= 760
+      'excellent'
+    elsif credit_score >= 725
+      'great'
+    elsif credit_score >= 660
+      'good'
+    elsif credit_score >= 560
+      'medicore'
+    else
+      'bad'
+    end
+  end
+
 end
