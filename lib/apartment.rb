cefb6e444b804cbe20eba74709317834cc1f2c74
Emma Gardner <emmagardner@outlook.com>
2014-06-13 18:00:42 -0400

    make adjustment to add_tenant and remove_tenant: they work!

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,19 +1,12 @@
-require 'pry'
 
-
-
-
-
-
-#has a method that removes all tenants
 #has an average credit score, calculated from all tenants
 #has a credit rating, calculated from the average credit score using the logic below
-
-
+require 'pry'
 
 class Apartment
   #has a number, rent, square footage, number of bedrooms, and number of bathrooms
-  attr_accessor number, rent, sq_footage, number_of_bedrooms, number_of_bathrooms
+  attr_accessor :number, :rent, :sq_footage, :number_of_bedrooms, :number_of_bathrooms
+  attr_reader :tenants
 
   def initialize(number, rent, sq_footage, number_of_bedrooms, number_of_bathrooms)
     @number = number
@@ -21,30 +14,28 @@ class Apartment
     @sq_footage = sq_footage
     @number_of_bedrooms = number_of_bedrooms
     @number_of_bathrooms = number_of_bathrooms
-    #has many tenants
-    #the list of tenants should not be modified directly (bonus: actually prevent it from being modified directly)
     @tenants = []
   end
 
   # add a tenant, but if it has bad credit or there are not enough bedrooms, report an error
-  def add_tenant(name)
-    tenants << name
-    if name.credit_rating = "bad"
+  def add_tenant(tenant) #(who ever calls this should pass a variable of a Tenant.new)
+    @tenants << tenant
+    if tenant.credit_rating == "bad"
       raise 'ERROR- DO NOT TRUST THIS TENANT'
     end
-    if tenants.count > number_of_bedrooms
+    if @tenants.count > number_of_bedrooms
       raise 'ERROR - TENANT OVERLOAD'
     end
   end
 
-  #has a method to remove a specific tenant either by object reference or by name (bonus: do this without checking classes), which raises an error if the tenant is not found
-
-  def remove_tenant(name)
-    tenants.delete_if? { |tenant| tenant.name == name }
-    if tenant.name != name
-      raise 'ERROR- NO TENANT FOUND'
-    end
+  def remove_tenant(tenant)
+    @tenants.delete(tenant) {'ERROR-NO TENANT FOUND'}
   end
 
-
+  #has a method that removes all tenants
+  def remove_all_tenants
+    @tenants.clear
+  end
 end
+
+binding.pry
