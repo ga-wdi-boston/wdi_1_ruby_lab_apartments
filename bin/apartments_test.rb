44d999078d16cb5c34086c35c05eea744fab5df3
Max Kohl <maxkohl810@gmail.com>
2014-06-13 15:55:46 -0400

    buildings can be successfully created
    	* also commited my spec list as well

--- bin/apartments_test.rb
+++ bin/apartments_test.rb
@@ -4,5 +4,7 @@ require_relative '../lib/apartment.rb'
 require_relative '../lib/building.rb'
 require_relative '../lib/tenant.rb'
 
+brook = Building.new(address: '59 Manchester Road, Brookline, MA, 02446')
+
 
 binding.pry
--- lib/building.rb
+++ lib/building.rb
@@ -1,2 +1,12 @@
 class Building
+  attr_reader :apartments
+
+  def initialize(address:)
+    @address = address
+    @apartments = []
+  end
+
+  def new_apartment
+    @apartments << Apartment.new
+  end
 end
