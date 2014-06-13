cfec849cbc402da2cff79048c555a03ace47ddc6
sarahchristine913 <s.mele13@gmail.com>
2014-06-13 17:04:44 -0400

    Created Apartment Class, with test file
    * Added new class def for apartment
      * Added initialize methods with required (not optional) params
      * Added add tenant method
    
    * Added apartment test file
      * Successfully tested apartment with initialize method.
    
      *Adding tenant is still untested
    
    * Added more test examples to tenant test file

--- bin/tenant_test.rb
+++ bin/tenant_test.rb
@@ -9,3 +9,5 @@ smuggler = Tenant.new("Sam Smuggler", 22, 520)
 dr_melissa = Tenant.new("Dr. Melissa Lawson", 34, 780)
 
 puts "#{jonie.name}'s credit score is #{jonie.credit_score} with a #{jonie.credit_rating} credit rating."
+
+puts "#{smuggler.name}'s credit score is #{smuggler.credit_score} with a #{smuggler.credit_rating} credit rating."
--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,9 +1,53 @@
-# Apartment
-# * has a number, rent, square footage, number of bedrooms, and number of bathrooms
-# * has many tenants
-# * the list of tenants should not be modified directly (bonus: actually prevent it from being modified directly)
-# * has a method to add a tenant that raises an error if the tenant has a "bad" credit rating, or if the new tenant count would go over the number of bedrooms
-# * has a method to remove a specific tenant either by object reference *or* by name (bonus: do this without checking classes), which raises an error if the tenant is not found
+# !! Apartment
+# !!has a number, rent, square footage, number of bedrooms, and number of bathrooms
+# !!has many tenants
+# !!the list of tenants should not be modified directly
+# *****(bonus: actually prevent it from being modified directly)*****
+# !!has a method to add a tenant that raises an error if the tenant has a "bad" credit rating, or if the new tenant count would go over the number of bedrooms
+# has a method to remove a specific tenant either by object reference *or* by name (bonus: do this without checking classes), which raises an error if the tenant is not found
 # * has a method that removes all tenants
 # * has an average credit score, calculated from all tenants
 # * has a credit rating, calculated from the average credit score using the logic below
+
+require_relative 'tenant'
+
+class Apartment
+
+  attr_accessor :number, :rent, :sq_ft, :bedrooms, :baths
+  attr_reader :all_tenants
+
+  def initialize(number, rent, sq_ft, bedrooms, baths)
+    @number, @rent, @sq_ft, @bedrooms, @baths = number, rent, sq_ft, bedrooms, baths
+    @all_tenants = []
+  end
+
+  def add_tenant(tenant)
+    if credit_rating == "bad"
+      raise "Error: Tenant cannot be added: Credit too low"
+    end
+
+    if bedrooms < tenant.count
+      raise "Error: Tenant cannot be added: No occupancy"
+    end
+
+    all_tenants.push(tenant)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
