3aad360001902e21948a00652785a3098e06b01e
Julie <juliesof@gmail.com>
2014-06-13 18:05:47 -0400

    Added credit score averaging to the apartment class

--- bin/test_case1.rb
+++ bin/test_case1.rb
@@ -1,11 +1,19 @@
 #require '../lib/building.rb'
-#require '../lib/apartment.rb'
+require '../lib/apartment.rb'
 require '../lib/tenant.rb'
 require 'pry'
 
-julie = Tenant.new(first_name: 'Julie', last_name: 'Soforenko', age: 29, credit_score: 740)
+julie = Tenant.new(first_name: 'Julie', last_name: 'Soforenko', age: 29, credit_score: 640)
+carrie = Tenant.new(first_name: 'Carrie', last_name: 'Ziskind', age: 27, credit_score: 780)
+mikey = Tenant.new(first_name: 'Michael', last_name: 'Soforenko', age: 24, credit_score: 700)
+sarah = Tenant.new(first_name: 'Sarah', last_name: 'Colen', age: 28, credit_score: 600)
+rachel = Tenant.new(first_name: 'Rachel', last_name: 'Colen', age: 24, credit_score: 540)
+
+individual_tenants = %w(julie carrie mikey sarah rachel)
+individual_tenants.evict_all
 
 credit_rating_1 = julie.create_credit_rating(credit_score: 740)
-puts credit_rating_1
+
+
 
 binding.pry
--- lib/apartment.rb
+++ lib/apartment.rb
@@ -6,7 +6,8 @@ class Apartment < Tenant
   attr_accessor :rent, :num_tenants #not sure if tenants should go here
   attr_reader :apt_num, :sq_footage, :num_bedrooms, :num_bathrooms
 
-  def initialize(apt_num:, sq_footage:, num_bedrooms:, num_bathrooms: rent:, num_tenants:)
+  def initialize(apt_num:, sq_footage:, num_bedrooms:, num_bathrooms:, rent:, num_tenants:)
+    super
     @apt_num = apt_num
     @sq_footage = sq_footage
     @num_bedrooms = num_bedrooms
@@ -19,6 +20,17 @@ class Apartment < Tenant
   def tenant_error(credit_score:, num_bedrooms:)
     raise "Warning! Shitty tenant. Do not offer a lease."
     raise "Warning! There are not enough bedrooms. Do not offer lease."
+
+  rescue
+  end
+
+  def evict_all #can't test until i have an array of tenants
+    if its not empty or eviction
+    individual_tenants.map.delete
+  end
+
+  def avg_credit
+    individual_tenants.credit_rating.reduce(&:+)/individual_tenants.length
   end
 
 
