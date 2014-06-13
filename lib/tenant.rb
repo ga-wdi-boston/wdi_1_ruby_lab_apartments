cd317110a7d1fb5f2bb66b848f819940a35443c7
Emma Gardner <emmagardner@outlook.com>
2014-06-13 16:28:41 -0400

    set up apartment class

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,15 +1,6 @@
-# has a name, age, and credit score
-# has a credit rating, calculated from the credit score as follows:
-# 760 or higher is "excellent"
-# 725 or higher is "great"
-# 660 or higher is "good"
-# 560 or higher is "mediocre"
-# anything lower is "bad"
-
 require 'pry'
 
 class Tenant
-
   attr_accessor :name, :age, :credit_score
 
   def initialize(name, age, credit_score)
@@ -31,9 +22,7 @@ class Tenant
     else
       "excellent"
     end
-
   end
-
 end
 
 binding.pry
