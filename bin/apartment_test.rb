286eefa370c1d438060d7c7eb78ca0c2a18e3d5c
brimatteng <btenggren20@gmail.com>
2014-06-13 14:57:21 -0400

    set up a working tenant class with credit score method and add test class

---
+++ bin/apartment_test.rb
@@ -0,0 +1,4 @@
+require 'pry'
+require_relative '../lib/tenant'
+
+binding.pry
---
+++ lib/tenant.rb
@@ -0,0 +1,32 @@
+### `Tenant`
+# * has a name, age, and credit score
+# * has a credit rating, calculated from the credit score as follows:
+#   * 760 or higher is "excellent"
+#   * 725 or higher is "great"
+#   * 660 or higher is "good"
+#   * 560 or higher is "mediocre"
+#   * anything lower is "bad"
+
+class Tenant
+  attr_reader :name, :age, :credit_score
+  def initialize(namer:, ager:, credit_scorer:)
+    @name = namer
+    @age = ager
+    @credit_score = credit_scorer
+  end
+
+  def credit_score_rating
+    if credit_score >= 760
+      @credit_score_rating = 'excellent'
+    elsif credit_score >= 725
+      @credit_score_rating = 'great'
+    elsif credit_score >= 660
+      @credit_score_rating = 'good'
+    elsif credit_score >=560
+      @credit_score_rating ='mediocre'
+    else @credit_score_rating = 'bad'
+    end
+    @credit_score_rating
+  end
+
+end
