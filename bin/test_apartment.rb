11db0438eb3e60a7bda7ed926d748984dc75333d
Jodi Detch <jodidetch@gmail.com>
2014-06-15 12:48:30 -0400

    Added credit_rating method to apartment file, but not working exactly how I want, so commented out for now

--- bin/test_apartment.rb
+++ bin/test_apartment.rb
@@ -5,5 +5,8 @@ tenant = Tenant.new('Jodi', 28, 550)
 
 puts "My apartment is number #{apartment.number}, it costs #{apartment.rent} per month, is #{apartment.square_footage} square feet, has #{apartment.bedrooms} bedrooms and #{apartment.bathrooms} bathrooms."
 
-puts "I am adding a new tenant named #{apartment.add_tenant('John Smith')} to my apartment."
+#puts "I am adding a new tenant named #{apartment.add_tenant('John Smith')} to my apartment."
 
+puts "I am adding a new tenant named #{apartment.add_tenant('tenant')} to my apartment."
+
+puts "#{tenant.credit_rating}"
