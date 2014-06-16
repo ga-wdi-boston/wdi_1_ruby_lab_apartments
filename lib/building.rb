5b89e4e59a3325b792f9d51286d29da2f886162c
Emma Gardner <emmagardner@outlook.com>
2014-06-15 22:23:29 -0400

    test building class for initializer- check
    test add apt - check
    
    test remove apt - error
    
    test total sqaure feet - check
    
    test monthly revenue - check

--- lib/building.rb
+++ lib/building.rb
@@ -1,47 +1,30 @@
 require 'pry'
 
-
-
-
-
-
-
-
-
-# has a method to retrieve all apartments grouped by credit rating (bonus: sort the groups by credit score)
-
-
 class Building
-  attr_accessor address:
+  attr_accessor :address
+  attr_reader :apts
 
   def initialize(address)
-    # has an address
     @address = address
-    # has many apartments (array)
-    # the list of apartments should not be modified directly (bonus: actually prevent it from being modified directly)
     @apts = []
   end
 
-  # has a method to add an apt
-  def add_apt(apt) #(who ever calls this should pass a variable of an Apartment.new)
+  def add_apt(apt)
     @apts.push(apt)
   end
 
  #has a method to remove a specific apartment by its number, which raises an error if the number is not found or the apartment currently has any tenants (bonus: allow overriding this constraint)
   def remove_apt(number)
-    @apts.delete_if{ |apt| apt.number == number)
+    @apts.delete_if{ |apt| apt.number == number}
     if @apts.include?(number) == false
       raise 'ERROR-NO APARTMENT NUMBER FOUND'
     end
   end
 
-  # has a total square footage, calculated from all apartments
-  # needs to take the sq footage from each object in the apts array and add together
   def total_sq_feet
     apts.map{ |apt| apt.sq_footage }.reduce(:+)
   end
 
-  # has a total monthly revenue, calculated from all apartment rents
   def monthly_revenue
     apts.map{ |apt| apt.rent }.reduce(:+)
   end
@@ -51,6 +34,17 @@ class Building
   def tenant_list(tenants) #(whoever call this needs to pass the Variable of the Apartment.new.tenants array)
     tenants.select{ |tenant| tenant.name }
   end
+
+# has a method to retrieve all apartments grouped by credit rating (bonus: sort the groups by credit score)
+def apt_credit_rating(tenants)
+  # go into the tenants array and sort the objects by credit rating, then put into new arrays
+  tenants.select{ |tenant| tenant.credit_rating }.sort
+
+end
+
+
+end
+
 
 
 binding.pry
