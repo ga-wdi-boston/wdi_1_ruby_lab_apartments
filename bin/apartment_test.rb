ba11d85a4e6eb6bc09b0800c14b2cbc25dbe2ade
sarahchristine913 <s.mele13@gmail.com>
2014-06-16 09:15:49 -0400

    Updated Apartment Methods
    * Added remove tenant, remove all, average credit rating and credit score method
    * Updated apartment test file to test newly added methods

--- bin/apartment_test.rb
+++ bin/apartment_test.rb
@@ -10,9 +10,17 @@ smuggler = Tenant.new("Sam Smuggler", 22, 520)
 
 dr_melissa = Tenant.new("Dr. Melissa Lawson", 34, 780)
 
-puts "Smug's credit rating is "
+# puts "Smug's credit rating is "
 
-basement_studio.add_tenant(smuggler)
+basement_studio.add_tenant(dr_melissa)
 
 puts "#{basement_studio.all_tenants}"
 
+puts "#{basement_studio.avg_score}"
+
+puts "#{basement_studio.avg_credit_rating}"
+
+basement_studio.remove_tenant(smuggler)
+
+
+puts "#{basement_studio.all_tenants}"
--- lib/apartment.rb
+++ lib/apartment.rb
@@ -4,10 +4,10 @@
 # !!the list of tenants should not be modified directly
 # *****(bonus: actually prevent it from being modified directly)*****
 # !!has a method to add a tenant that raises an error if the tenant has a "bad" credit rating, or if the new tenant count would go over the number of bedrooms
-# has a method to remove a specific tenant either by object reference *or* by name (bonus: do this without checking classes), which raises an error if the tenant is not found
-# * has a method that removes all tenants
-# * has an average credit score, calculated from all tenants
-# * has a credit rating, calculated from the average credit score using the logic below
+# !!has a method to remove a specific tenant either by object reference *or* by name (bonus: do this without checking classes), which raises an error if the tenant is not found
+# * !!has a method that removes all tenants
+# * !!has an average credit score, calculated from all tenants
+# * !!has a credit rating, calculated from the average credit score using the logic below
 
 require_relative 'tenant'
 
@@ -31,6 +31,39 @@ class Apartment
     end
 
     @all_tenants.push(tenant)
+  end
+
+  def remove_tenant(tenant)
+    if all_tenants.any?
+    @all_tenants.delete(tenant)
+      # if nil
+      #   raise "Error: Tenant cannot be found in Unit #{number}."
+      # else
+      #   return "Tenant #{tenant} removed successfully."
+      # end
+    end
+  end
+
+  def remove_all
+    @all_tenants = []
+  end
+
+  def avg_score
+    (all_tenants.map(&:credit_score).reduce(0, &:+))/(all_tenants.count)
+  end
+
+  def avg_credit_rating
+    if avg_score > 759
+     :excellent
+    elsif avg_score > 725
+     :great
+    elsif avg_score > 660
+     :good
+    elsif avg_score > 560
+     :mediocre
+    else
+      :bad
+    end
   end
 
 end
